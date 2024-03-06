c1b99da757b61d3d1b3e6cb40977481f32ed3156
strep <sva@confident.ru>
2024-03-06 18:34:25 +0300

    Орг.коммит

---
+++ build/CMakeFiles/harvest_sweeper.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for harvest_sweeper.
+# This may be replaced when dependencies are built.
---
+++ build/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#define TEST_VAR  0
+
+int main() { std::cout << "Harvest Sweeper at your service!" << std::endl; }

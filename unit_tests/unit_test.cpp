708ad9f24faf07ff041a84098fe739a5951055cf
strep <sva@confident.ru>
2024-03-07 16:00:23 +0300

    Реорганизовать дерево проекта

---
+++ unit_tests/unit_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "gmock/gmock.h"
+
+class InitTest {};
+
+TEST(InitTest, InitTest) { EXPECT_EQ(1, 1); }
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

aa38f94710960392a00922710cce31eac3685d87
strep <sva@confident.ru>
2024-03-11 17:06:18 +0300

    Add rule for code coverage generation

--- sources/cmd_parser.cpp
+++ sources/cmd_parser.cpp
@@ -14,8 +14,21 @@ CmdParser::CmdParser(int argc, const char **argv) {
   desc.add_options()("help,h", "Показать справку")(
       "object_list", po::value<std::string>(), "JSON-файл с объектами")(
       "output_dir", po::value<std::string>(), "Куда сохранить результат");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+
+  if (vm.count("help")) {
+    m_isHelpRequested = true;
+  }
+
+  if (vm.count("object_list")) {
+
+  }
+
+  if (vm.count("output_dir")) {
+    
+  }
 }
 
 CmdParser::~CmdParser() {}
-
-bool CmdParser::isArgumentsValid() const { return m_isArgumentsValid; }
--- sources/cmd_parser.hpp
+++ sources/cmd_parser.hpp
@@ -6,10 +6,12 @@ public:
   CmdParser(int argc, const char **argv);
   ~CmdParser();
 
-  bool isArgumentsValid() const;
+  bool isArgumentsValid() const { return m_isArgumentsValid; };
+  bool isHelpRequested() const { return m_isHelpRequested; };
 
 private:
   bool m_isArgumentsValid = false;
+  bool m_isHelpRequested = false;
 };
 
 #endif // CMD_PARSER_HPP

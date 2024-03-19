12864651796f73117ad3e0d03f83718caab09999
v-str <vyacheslav.strepetov@yandex.ru>
2024-03-19 20:48:28 +0300

    Org. commit

--- sources/sweep_worker.hpp
+++ sources/sweep_worker.hpp
@@ -1,9 +1,10 @@
 #ifndef SWEEP_WORKER_HPP
 #define SWEEP_WORKER_HPP
 
+#include <filesystem>
 #include <ranges>
 #include <string>
-#include <unordered_map>
+#include <vector>
 
 #include "tools.hpp"
 
@@ -16,14 +17,37 @@ public:
   ~SweepWorker() = default;
 
   void run() {
+    if (!std::filesystem::exists(m_outputDirPath)) {
+      Logger::error("Ошибка", m_outputDirPath + " не существует");
+      return;
+    }
+
     for (const auto &[key, value] : m_chunkView) {
       Logger::info(key, value);
+      m_currentFile = key;
+      m_currentFilePath = value;
     }
   }
 
 private:
+  void findFileDependencies() {
+    /*
+    если файл m_currentFile существует в начальном каталоге
+      если файла не существует в конечном каталоге
+        узнать тип файла, бинарный или нет
+          если бинарный, то
+            найти зависимости этого файла и записать их в вектор
+
+
+
+    */
+  }
+
   const string m_outputDirPath;
   ChunkView m_chunkView;
+
+  string m_currentFile;
+  string m_currentFilePath;
 };
 
 #endif // SWEEP_WORKER_HPP
